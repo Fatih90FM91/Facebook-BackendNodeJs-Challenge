ad2e7a977d24095f0822603ef8fb0b375e232322
Fatih90FM91 <hasan9091.fs@gmail.com>
2022-01-21 17:20:28 +0100

    updated

--- router.js
+++ router.js
@@ -7,8 +7,6 @@ router.get('/', controller.getHomePage);
 router.all('/new-feed', controller.getNewFeed);
 router.get('/show-feed/:id' , controller.showFeedPage);
 router.get('/delete-feed/:id', controller.delFeed);
-// router.get('/edit-article/:id', controller.getEditArticle);
-// router.post('/edit-article/:id', controller.postEditArticle);
 router.all('/edit-feed/:id', controller.updateFeed);
 
 module.exports = router;
